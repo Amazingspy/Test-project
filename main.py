34bb5727edb28e38aa28d8ffc51710f31396b48b
Amazingspy <amazingspidey348@gmail.com>
2024-02-21 22:44:00 +0530

    data modified

--- main.py
+++ main.py
@@ -7,20 +7,15 @@ conn = sqlite3.connect('mydatabase.db')
 cursor = conn.cursor()
 
 # Execute a SQL query to create a table
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                    id INTEGER PRIMARY KEY,
-                    name TEXT NOT NULL,
-                    email TEXT NOT NULL UNIQUE
-                )''')
-print("Table created successfully")
-
-# Execute a SQL query to insert data into the table
+# cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+#                     id INTEGER PRIMARY KEY,
+#                     name TEXT NOT NULL,
+#                     email TEXT NOT NULL UNIQUE
+#                 )''')
+# print("Table created successfully")
 cursor.execute('''INSERT INTO users (name, email) VALUES (?, ?)''',
                ('John Doe', 'john@example.com'))
-cursor.execute('''INSERT INTO users (name, email) VALUES (?, ?)''',
-               ('Jane Smith', 'jane@example.com'))
-cursor.execute('''INSERT INTO users (name, email) VALUES (?, ?)''',
-               ('Joe', 'joe@example.com'))
+print(f"1 record inserted: {cursor.rowcount} rows affected")
 
 # Commit the changes
 conn.commit()
